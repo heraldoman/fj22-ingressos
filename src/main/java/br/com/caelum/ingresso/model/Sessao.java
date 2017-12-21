89dc6c22c044d6bd544ff140dde9b763dbdac777
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-12-21 20:01:28 -0200

    Adiciona preco da sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -21,6 +22,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	
+	private BigDecimal preco;
+	
 	public Sessao() {
 	}
 	
@@ -29,6 +32,7 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.setPreco(sala.getPreco().add(filme.getPreco()));
 	}
 
 
@@ -62,6 +66,16 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 	
 	
